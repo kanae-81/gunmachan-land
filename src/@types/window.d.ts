bae7dfec7081f08b46be76ea7147a0a286ccf7f9
kanae-81 <kanae.jp81@gmail.com>
2022-01-29 18:09:05 +0900

    init: ts環境構築

---
+++ src/@types/window.d.ts
@@ -0,0 +1,4 @@
+interface Window {
+  Gchan: any;
+}
+declare var window: Window;
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { a } from "./module";
+
+(function (global, factory) {
+  factory(global);
+})(window, function (window: Window) {
+  const version = "1.0.0";
+  const Gchan = () => {
+    return {
+      version,
+    };
+  };
+
+  window.Gchan = Gchan;
+  return Gchan;
+});
