a84d112722f81dd7517174dd5c4517c90fe6f8dd
kanae-81 <kanae.jp81@gmail.com>
2022-01-29 18:19:22 +0900

    init: テストの導入

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { a } from "./module";
 
 (function (global, factory) {
   factory(global);
+  a();
 })(window, function (window: Window) {
   const version = "1.0.0";
   const Gchan = () => {
---
+++ tests/index.test.ts
@@ -0,0 +1,16 @@
+import { a } from "../src/module/index";
+
+describe("a()のテスト", () => {
+  it("test と出力", () => {
+    const log = jest.spyOn(console, "log").mockReturnValue();
+
+    // a メソッドの実行
+    a();
+
+    // 1番目のログ出力が 'Hello.' と一致するかチェック
+    expect(log).toHaveBeenNthCalledWith(1, "test");
+
+    // 'jest.spyOn()' によって作成されたモックをリセットします
+    log.mockRestore();
+  });
+});
